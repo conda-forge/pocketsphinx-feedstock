7871de00848b35391226a3d05ccc4a4a8a002681
Curtis Hampton <CurtLHampton@gmail.com>
2018-07-29 21:33:51 -0400

    iterate on build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 ls -lh
 
-pushd pocketsphinx
+pushd model
 
 ./autogen.sh
 ./configure --prefix="$PREFIX"
 make clean all
 make check
 make install
+
 popd
 
-pushd pocketsphinx
-cp -avf pocketsphinx/model/*  $PREFIX/share/model
+cp -avf model/*  $PREFIX/share/model
 
