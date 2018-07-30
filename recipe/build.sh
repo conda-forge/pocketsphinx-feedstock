5421f1f2fa7bec5bec8401c7e875ec5b3e508e86
Curtis Hampton <CurtLHampton@gmail.com>
2018-07-29 21:39:14 -0400

    iterate

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-ls -lh
 
-pushd model
+cp -avf model/*  $PREFIX/share/model
 
 ./autogen.sh
 ./configure --prefix="$PREFIX"
 make clean all
 make check
 make install
-
-popd
-
-cp -avf model/*  $PREFIX/share/model
-
