f111f96178baddbfb6af105fb388eda5f2f302ef
Curtis Hampton <CurtLHampton@gmail.com>
2018-07-29 21:02:38 -0400

    Update build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+pushd pocketsphinx-5prealpha
+
 ./autogen.sh
 ./configure --prefix="$PREFIX"
 make clean all
 make check
 make install
+popd
+
+pushd pocketsphinx-5prealpha
+cp -avf pocketsphinx-5prealpha/model/*  $PREFIX/share/model
+
